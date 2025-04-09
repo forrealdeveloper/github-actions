534eb8fdbd866eeec30c237fffcf12c103b38f9b
Parth <parthsali04@gmail.com>
2025-04-09 14:37:29 +0530

    Apply some changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("Home Route is working fine");
