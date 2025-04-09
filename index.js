071394a1151765fed3e02d07b9caf970bd671ac7
Parth <parthsali04@gmail.com>
2025-04-09 14:23:08 +0530

    Add deploy image workflow

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Home Route is working fine");
+});
+
+app.get("/health", (req, res) => {
+  res.send("Health Route is working fine");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
